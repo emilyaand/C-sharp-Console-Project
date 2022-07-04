bba4d2915b45e04a79c60452f7eaf8f8a9e7b69c
emilyaand <104448537+emilyaand@users.noreply.github.com>
2022-07-04 18:17:30 +0400

    Add files via upload

--- Pharmacy.cs
+++ Pharmacy.cs
@@ -6,29 +6,63 @@ using System.Threading.Tasks;
 
 namespace Console_Project
 {
-    internal class Pharmacy
+    public class Pharmacy
     {
         public string Id { get; set; }
         public string Name { get; set; }
         public string MinSalary { get; set; }
         public string Budget { get; set; }
         public string Location { get; set; }
+        public string Username { get; internal set; }
+       // public Employee[] employee;
+      //  public Drug[] drug;
     }
-    class Employee: Pharmacy
+    public class Employee: Pharmacy
     {
         public int IdE { get; set; }
         public string NameE { get; set; }
-        public string RoleType { get; set; } //(Admin, Staff)
+        public string RoleType {
+            get { return RoleType; }
+            set { RoleType = value; }
+                } //(Admin, Staff)
         public string Surname { get; set; }
         public int BirthDate { get; set; }
         public int Salary { get; set; }
-        public string Username { get; set; }//(uniqiue) 
-        public string Password { get; set; }//(uzunlugu 5den az olmayan, icinde en azi 1 reqem bir boyuk herf ve bir xarakter olmalidir)
+        public string UsernameE {
+            get { return UsernameE; }
+            set { UsernameE = value; }
+               }//(uniqiue) 
+        public string PasswordE
+        {
+            get { return PasswordE; }
+            set { PasswordE = value; }
+
+        }//(uzunlugu 5den az olmayan, icinde en azi 1 reqem bir boyuk herf ve bir xarakter olmalidir)
+        public bool IsPasswordValid()
+        {
+            if (PasswordE.Length < 5) return false;
+
+            bool IsDigitIncluded = false;
+            foreach    (char item in PasswordE)
+            {
+                if(int.TryParse(item+"", out int result))
+                {
+                    IsDigitIncluded = true;
+                    break;
+                }
+            }
+            if (IsDigitIncluded == false) return false;
+            return true;
+        }
+
     }
-    class Drug: Pharmacy    {
+    public class Drug: Pharmacy    {
         public int IdP { get; set; }
         public string NameP { get; set; }
-        public string DrugType { get; set; } //(syrob, powder, tablet)
+        public string DrugType {
+            get { return DrugType; }
+            set { DrugType = value; }
+                } //(syrob, powder, tablet)
         public int Count { get; set; } 
         public int PurchasePrice { get; set; } 
         public int SalePrice { get; set; } 
--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Console_Project
 {
@@ -6,7 +7,42 @@ namespace Console_Project
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            
+            Employee employee;
+            Drug drug;
+            employee = new Employee();
+            employee.Name = "admin";
+            employee.UsernameE = "Superadmin";
+            employee.PasswordE = "admin123";
+            Console.Clear();
+
+            List<Employee> users = new List<Employee>();
+            users.Add(new Employee { UsernameE = "Superadmin", PasswordE = "admin123" });
+            
+            bool CorrectLogin = Login(users);
+            while (!CorrectLogin)
+                {
+                Console.WriteLine("The wromg Username or Password. Please try again");
+                Login(users);
+                }
+
+                   
+        }
+        static bool Login(List<Employee> users)
+        {
+            Console.WriteLine("Please insert your Username: ");
+            string Username = Console.ReadLine();
+            Console.WriteLine("Indicate your Password: ");
+            string Password = Console.ReadLine();
+
+            Employee employee = users.Find(u => u.UsernameE == Username && u.PasswordE == Password);
+            if (employee != null)
+            {
+               
+                return false;
+            }
+            else return true;
+
         }
     }
 }
